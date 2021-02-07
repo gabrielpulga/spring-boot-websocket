ae885d929fa64cef1816087947c67f748f275556
Gabriel Pulga <gabriel.pulga@codefiction.net>
2021-02-07 13:04:29 -0300

    config, controller and model classes

---
+++ src/main/java/com/tupinamba/springbootwebsocket/controller/ChatController.java
@@ -0,0 +1,26 @@
+package com.tupinamba.springbootwebsocket.controller;
+
+import com.tupinamba.springbootwebsocket.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat.register")
+    @SendTo("/topic/public")
+    public ChatMessage register(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.send")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+}
